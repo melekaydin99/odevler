a8c6457f0e8e045cbd5c5655c3b83e29ab2378e7
melekaydin99 <49302547+melekaydin99@users.noreply.github.com>
2019-04-19 16:41:45 +0300

    Add files via upload

---
+++ sınırsız çapraz kayan button.a/sınırsız kayan/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sınırsız_kayan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (button1.Location.X + button1.Size.Width > this.Width)
+            {
+                button1.Location = new Point(0 - button1.Size.Width, button1.Location.Y);
+            }
+
+            else
+            {
+                button1.Location = new Point(button1.Location.X+10,button1.Location.Y+5);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
