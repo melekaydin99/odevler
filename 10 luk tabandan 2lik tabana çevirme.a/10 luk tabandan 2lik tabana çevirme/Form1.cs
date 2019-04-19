48bc202323dd873062010f6737ceaead58fb074c
melekaydin99 <49302547+melekaydin99@users.noreply.github.com>
2019-04-19 13:46:37 +0300

    Add files via upload

---
+++ 10 luk tabandan 2lik tabana çevirme.a/10 luk tabandan 2lik tabana çevirme/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _10_luk_tabandan_2lik_tabana_çevirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+        int mod;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            sayi = Convert.ToInt32(textBox1.Text);
+
+            while (sayi > 1)
+            {
+               
+                mod = sayi % 2;
+                sayi = sayi / 2;
+                label1.Text = mod.ToString()+label1.Text;
+            }
+
+            label1.Text = sayi.ToString()+label1.Text;
+        }
+    }
+}
