f508bdcb031d5037d91ee481f0a37ae09ea364f7
melekaydin99 <49302547+melekaydin99@users.noreply.github.com>
2019-05-25 19:27:10 +0300

    Add files via upload

---
+++ Sudoku.a/Sudoku/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(59, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 51);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(59, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(78, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(59, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(151, 51);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(59, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(6, 84);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(59, 20);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(78, 84);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(59, 20);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(151, 84);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(59, 20);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox9);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 125);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "DENE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(249, 31);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(62, 20);
+            this.textBox10.TabIndex = 11;
+            this.textBox10.Visible = false;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(249, 63);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(62, 20);
+            this.textBox11.TabIndex = 12;
+            this.textBox11.Visible = false;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(249, 96);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(62, 20);
+            this.textBox12.TabIndex = 13;
+            this.textBox12.Visible = false;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(18, 149);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(59, 20);
+            this.textBox13.TabIndex = 14;
+            this.textBox13.Visible = false;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(90, 149);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(59, 20);
+            this.textBox14.TabIndex = 15;
+            this.textBox14.Visible = false;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(163, 149);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(59, 20);
+            this.textBox15.TabIndex = 16;
+            this.textBox15.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 385);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ Sudoku.a/Sudoku/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random salla = new Random();
+        int[] sayi = new int[9];
+        int sayac = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //satır toplamları
+
+            int satir1 = 0, satir2 = 0, satir3 = 0;
+            satir1 = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text) + Convert.ToInt32(textBox3.Text);
+            textBox10.Text = satir1.ToString();
+
+
+            satir2 = Convert.ToInt32(textBox4.Text) + Convert.ToInt32(textBox5.Text) + Convert.ToInt32(textBox6.Text);
+            textBox11.Text = satir2.ToString();
+
+            satir3 = Convert.ToInt32(textBox7.Text) + Convert.ToInt32(textBox8.Text) + Convert.ToInt32(textBox9.Text);
+            textBox12.Text = satir3.ToString();
+
+            //sutun toplamları
+
+            int sutun1 = 0, sutun2 = 0, sutun3 = 0;
+            sutun1 = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox4.Text) + Convert.ToInt32(textBox7.Text);
+            textBox13.Text = sutun1.ToString();
+
+            sutun2 = Convert.ToInt32(textBox2.Text) + Convert.ToInt32(textBox5.Text) + Convert.ToInt32(textBox8.Text);
+            textBox14.Text = sutun2.ToString();
+
+            sutun3 = Convert.ToInt32(textBox3.Text) + Convert.ToInt32(textBox6.Text) + Convert.ToInt32(textBox9.Text);
+            textBox15.Text = sutun3.ToString();
+
+
+            if (satir1 == 15 && satir2 == 15 && satir3 == 15 && sutun1 == 15 && sutun2 == 15 && sutun3 == 15)
+            {
+                MessageBox.Show("Tebrikler kazandınız");
+            }
+            else
+            {
+                MessageBox.Show("tekrar deneyiniz!");
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*textBox1.Text = "0";
+           textBox2.Text = "0";
+           textBox3.Text = "0";
+           textBox4.Text = "0";
+           textBox5.Text = "0";
+           textBox6.Text = "0";
+           textBox7.Text = "0";
+           textBox8.Text = "0";
+           textBox9.Text = "0";*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox1.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox1.Clear();
+                    textBox1.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    sayac++;
+                    textBox2.Focus();
+                }
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox2.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox1.Clear();
+                    textBox2.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    sayac++;
+                    textBox3.Focus();
+
+                }
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox3.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox3.Clear();
+                    textBox3.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    textBox4.Focus();
+                    sayac++;
+                }
+
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox4.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox4.Clear();
+                    textBox4.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    sayac++;
+                    textBox5.Focus();
+
+                }
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox5.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox5.Clear();
+                    textBox5.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    sayac++;
+                    textBox6.Focus();
+
+                }
+            }
+
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox6.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox6.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox6.Clear();
+                    textBox6.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    textBox7.Focus();
+                    sayac++;
+                }
+
+            }
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox7.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox7.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox7.Clear();
+                    textBox7.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    textBox8.Focus();
+                    sayac++;
+                }
+            }
+
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox8.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox8.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox8.Clear();
+                    textBox8.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    textBox9.Focus();
+                    sayac++;
+                }
+            }
+
+        }
+
+
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox9.Text != "")
+            {
+                int s1;
+                s1 = Convert.ToInt32(textBox9.Text);
+
+                if (sayi.Contains(s1))
+                {
+                    textBox9.Clear();
+                    textBox9.Focus();
+                    MessageBox.Show("Aynı değeri tekrar girdiniz, değiştriniz!");
+                }
+                else
+                {
+                    sayi[sayac] = s1;
+                    sayac++;
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                sayi[i] = 0;
+            }
+            sayac = 0;
+            textBox1.Text = salla.Next(1, 10).ToString();
+            textBox5.Text = salla.Next(1, 10).ToString();
+            textBox9.Text = salla.Next(1, 10).ToString();
+        }
+
+    }
+}
