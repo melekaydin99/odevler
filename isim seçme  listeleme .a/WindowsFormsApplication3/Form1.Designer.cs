7d519153ec9117b0d9312fe39760ab4d0ff52318
melekaydin99 <49302547+melekaydin99@users.noreply.github.com>
2019-05-25 18:58:37 +0300

    Add files via upload

---
+++ isim seçme  listeleme .a/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyad1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şanslı ismi seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(90, 173);
+            this.listBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ad,
+            this.soyad1,
+            this.yas});
+            this.dataGridView1.Location = new System.Drawing.Point(143, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(340, 181);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ad
+            // 
+            this.ad.HeaderText = "ad";
+            this.ad.Name = "ad";
+            // 
+            // soyad1
+            // 
+            this.soyad1.HeaderText = "soyad1";
+            this.soyad1.Name = "soyad1";
+            // 
+            // yas
+            // 
+            this.yas.HeaderText = "yas";
+            this.yas.Name = "yas";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "İsimleri listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 248);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyad1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yas;
+    }
+}
+
---
+++ isim seçme  listeleme .a/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] yarismaci = new string[] {"talha","samet","polat","muhammed","sema","sümeyye","melek","uğur","halit","dilek"};
+        string[] soyad = new string[]     {"aydın","güngör","kütük", "polat","özer","tekin","demir","kaya","efeler","yücel"};
+        Random salla = new Random();
+     // int sayac=0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for(int i=0; i< yarismaci.Length; i++)  //yarışmacı sayısı kadar dönmesi için lenght ekledik
+            {
+                listBox1.Items.Add(yarismaci[i]);
+            }
+
+            int kazanan;
+            kazanan = salla.Next(0,yarismaci.Length);
+            MessageBox.Show("kazanan isim=" + yarismaci[kazanan]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int a = 0; a < 100; a++)
+            {
+                dataGridView1.Rows.Add();
+                int kazanan, soyisim;
+                kazanan = salla.Next(0, yarismaci.Length);
+                soyisim = salla.Next(0, soyad.Length);
+
+                dataGridView1.Rows[a].Cells[0].Value = yarismaci[kazanan];  //kazananın adını ekler
+                dataGridView1.Rows[a].Cells[1].Value = soyad[soyisim];      //kazananın soyadını ekler
+            //    sayac++;  //butona kaç defa tıklandığını hesaplar
+
+
+                dataGridView1.Rows[a].Cells[2].Value = salla.Next(1,100);  
+            }
+
+
+        }
+    }
+}
